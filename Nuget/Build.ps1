cd3c32665a9714a08dbefb5b54390e37a016daf0
Ricardo Polo <ricardo.polo@tulpep.com>
2014-10-21 17:53:29 -0500

    Showing Progress

--- Nuget/Build.ps1
+++ Nuget/Build.ps1
@@ -1,3 +1,5 @@
 ﻿$nugetExePath = ".\nuget.exe"
+Write-Output "Downloading nuget.exe from nuget.org"
 Invoke-WebRequest "https://nuget.org/nuget.exe" -OutFile $nugetExePath
+Write-Output "Creating Nuget Package"
 Start-Process -FilePath $nugetExePath  -ArgumentList "pack specs.nuspec"
