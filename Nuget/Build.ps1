d5df67d2f279c0d343413e1f39e61bc4ce3a56ea
Ricardo Polo <ricardo.polo@tulpep.com>
2014-10-21 18:25:18 -0500

    Removed comment

--- Nuget/Build.ps1
+++ Nuget/Build.ps1
@@ -1,7 +1,7 @@
 ﻿$nugetExePath = ".\nuget.exe"
 Write-Output "Downloading nuget.exe from nuget.org"
-#Invoke-WebRequest "https://nuget.org/nuget.exe" -OutFile $nugetExePath 
+Invoke-WebRequest "https://nuget.org/nuget.exe" -OutFile $nugetExePath
 Write-Output "Creating Nuget Package"
-Start-Process -FilePath $nugetExePath  -ArgumentList "pack specs.nuspec" -Wait
+Start-Process -FilePath $nugetExePath  -ArgumentList "pack specs.nuspec"
 pwd
 Get-ChildItem
