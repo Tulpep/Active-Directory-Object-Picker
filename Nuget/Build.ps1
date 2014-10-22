28571d1d84989bbad8e64a55de672257ab38a055
Ricardo Polo <ricardo.polo@tulpep.com>
2014-10-21 19:01:55 -0500

    Getting version of build for nuget

--- Nuget/Build.ps1
+++ Nuget/Build.ps1
@@ -1,6 +1,16 @@
 ﻿$root = (split-path -parent $MyInvocation.MyCommand.Definition) + '\..'
-$nugetExePath = ".\nuget.exe"
 Write-Output "Downloading nuget.exe from nuget.org"
 Invoke-WebRequest "https://nuget.org/nuget.exe" -OutFile $root\Nuget.exe
+
+
+$version = [System.Reflection.Assembly]::LoadFile("$root\CubicOrange.Windows.Forms.ActiveDirectory\bin\Release\CubicOrange.Windows.Forms.ActiveDirectory.dll").GetName().Version
+$versionStr = "{0}.{1}.{2}" -f ($version.Major, $version.Minor, $version.Build)
+Write-Host "Setting .nuspec version tag to $versionStr"
+$content = (Get-Content $root\NuGet\MarkdownLog.nuspec)
+$content = $content -replace '\$version\$',$versionStr
+
+$content | Out-File $root\nuget\MarkdownLog.compiled.nuspec
+
+
 Write-Output "Creating Nuget Package"
 & $root\Nuget.exe pack $root\Nuget\specs.nuspec
