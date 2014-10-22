0ffb558f5550e3db5456c831e89ed31553cd05d6
Ricardo Polo <ricardo.polo@tulpep.com>
2014-10-21 19:05:04 -0500

    Changed bad rout of Nuget Specs

--- Nuget/Build.ps1
+++ Nuget/Build.ps1
@@ -6,10 +6,10 @@ Invoke-WebRequest "https://nuget.org/nuget.exe" -OutFile $root\Nuget.exe
 $version = [System.Reflection.Assembly]::LoadFile("$root\CubicOrange.Windows.Forms.ActiveDirectory\bin\Release\CubicOrange.Windows.Forms.ActiveDirectory.dll").GetName().Version
 $versionStr = "{0}.{1}.{2}" -f ($version.Major, $version.Minor, $version.Build)
 Write-Host "Setting .nuspec version tag to $versionStr"
-$content = (Get-Content $root\NuGet\MarkdownLog.nuspec)
+$content = (Get-Content $root\NuGet\specs.nuspec)
 $content = $content -replace '\$version\$',$versionStr
 
-$content | Out-File $root\nuget\MarkdownLog.compiled.nuspec
+$content | Out-File $root\nuget\specs.nuspec
 
 
 Write-Output "Creating Nuget Package"
